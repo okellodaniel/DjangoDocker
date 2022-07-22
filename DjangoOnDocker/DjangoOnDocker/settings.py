56cde13b28025b7910952110b3c274fb6becb0ad
okello daniel <handsdani@gmail.com>
2022-07-22 17:03:12 +0300

    Added File Uploads directories

--- DjangoOnDocker/DjangoOnDocker/settings.py
+++ DjangoOnDocker/DjangoOnDocker/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'upload'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
